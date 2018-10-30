e66f19c85e5895af89e961cad08c749d268a285e
desiml <desislava.mladenova355@gmail.com>
2018-10-30 16:36:05 +0200

    Exam1

---
+++ ExamOne1801681055/ExamOne1801681055/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamOne1801681055
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
